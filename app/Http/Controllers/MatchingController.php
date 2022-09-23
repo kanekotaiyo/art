9f496c7ee933933eb7222cf8f1be653397aed8c7
kanekotaiyo <taiyo.yokohama.0925@outlook.jp>
2022-09-23 21:36:52 +0900

    マッチング確定後、他のマッチング削除と関連の予約を非表示

--- app/Http/Controllers/MatchingController.php
+++ app/Http/Controllers/MatchingController.php
@@ -28,31 +28,40 @@ class MatchingController extends Controller
     
      public function matchlist(Matching $matching)
     {
+        /*$is_matching=$matching->where('confirmed', 1)->select('reserve_id')->get();
+        //dd($is_matching);
+        $is_matching_reserve=$matching->where('reserve_id',$is_matching)->get();
+        dd($is_matching_reserve);
+        if(isset($is_matching)){
+            return redirect('/matchlist');
+        }*/
         return view('matchlist')->with(['matchings' => $matching->getPaginateByLimitMatchlist(Auth::id())]);
     }
     
     public function show(Reserve $reserve, Matching $matching)
     {
-        //dd($reserve);
+        //dd($matching->get());
         $reserve_id=$reserve->id;
         //dd($reserve_id);
+        //$user_id=Auth::id();
+        
         return view('show')->with(['matchings' => $matching->getPaginateByLimitShow($reserve_id), 'reserve' => $reserve]);
     }
     
     public function matching_confirm(Reserve $reserve, Matching $matching)
     {
         $input=[
-            'confirmed'=>1,
+           'confirmed'=>1,
         ];
         $matching->fill($input)->save();
-        //dd($matching);
-        
+        Matching::where([["reserve_id",'=', $matching->reserve_id],["confirmed",'=',0]])->delete();
         return redirect('/myreserve/' . $matching->reserve->id);
     }
     
     public function delete_reserve(Reserve $reserve, Matching $matching)
     {
-        $de_matching=$matching->where('reserve_id',$reserve->id);
+        $de_matching=$matching->where('reserve_id',$reserve->id)->first();
+        //dd($de_matching);
         $de_matching->delete();
         //dd($de_matching);
         $reserve->delete();
@@ -64,4 +73,5 @@ class MatchingController extends Controller
         $matching->delete();
         return redirect('/matchlist');
     }
+    
 }
--- app/Reserve.php
+++ app/Reserve.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Matching;
 
 class Reserve extends Model
 {
@@ -35,6 +36,12 @@ class Reserve extends Model
     public function getPaginateByLimitAllreserve(int $user_id,int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
-        return $this->where('user_id','<>',$user_id)->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        $matched=Matching::where("confirmed",'=',1)->get(["reserve_id"]);
+        $array=[];
+        foreach($matched as $match){
+            array_push($array,$match->reserve_id);
+        }
+        //dd($array);
+        return $this->whereNotIn('id',$array)->where('user_id','<>',$user_id)->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
 }
