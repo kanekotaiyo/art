6a7cee5205383d6a14c2a91767b5e3364f15ee87
kanekotaiyo <taiyo.yokohama.0925@outlook.jp>
2022-10-26 19:51:50 +0900

    ログイン画面とヘッダーのデザイン変更とトップページの背景追加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('名前') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -54,7 +54,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('パスワード確認') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -65,7 +65,7 @@
                             <label for="car" class="col-md-4 col-form-label text-md-right">車の特徴</label>
 
                             <div class="col-md-6">
-                                <input id="car" type="text" class="form-control" name="car" required autocomplete="car">
+                                <input id="car" type="text" class="form-control" name="car" required autocomplete="car">*1
                             </div>
                         </div>
                         
@@ -73,19 +73,22 @@
                             <label for="comment" class="col-md-4 col-form-label text-md-right">コメント</label>
 
                             <div class="col-md-6">
-                                <input id="comment" type="text" class="form-control" name="comment" required autocomplete="comment">
+                                <input id="comment" type="text" class="form-control" name="comment" required autocomplete="comment">*1
                             </div>
                         </div>
                         <div>
-                            
                             <label for="car_image" class="col-md-4 col-form-label text-md-right">車の写真</label>
-                            <input type="file" name="car_image">
+                            <input type="file" name="car_image">*2
+                            <br>
+                            *1.コメントや車の特徴がなければ"なし"と書いてください
+                            <br>
+                            *2.車の写真は登録しなくても大丈夫です
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('アカウント作成') }}
                                 </button>
                             </div>
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'hitchhiking') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,15 +24,15 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'hitchhiking') }}
                 </a>
-                <div>
+                <div class="header">
                     <a href="/mypage">マイページ</a>
                 </div>
-                <div>
+                <div class="header">
                     <a href="/myreserve">予約状況</a>
                 </div>
-                <div>
+                <div class="header">
                     <a href="/allreserve">みんなの予約</a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -50,11 +50,11 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('ログイン') }}</a>
                             </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('アカウント作成') }}</a>
                                 </li>
                             @endif
                         @else
@@ -67,7 +67,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('ログアウト') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
